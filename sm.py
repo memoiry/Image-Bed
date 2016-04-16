524683053f18f55bc1e2acbe61dd9c7703807887
memoiry <memoiry@outlook.com>
2016-04-16 15:17:14 +0800

    source code

---
+++ sm.py
@@ -0,0 +1,42 @@
+
+import requests
+import sys
+import os
+import os.path
+
+if (len(sys.argv[1])>3):
+
+	url = 'https://sm.ms/api/upload';
+
+	path = '/users/xuguodong/desktop/figure/';
+	files = {'smfile' : open(path+sys.argv[1], 'rb')};
+
+	r = requests.post(url, files = files);
+
+	data1 = eval(r.text.encode('utf-8'));
+
+	url1 = data1['data']['url'];
+	print url1
+
+else:
+	dir = '/users/xuguodong/desktop/figure'
+	l = os.listdir(dir)
+	l.sort(key = lambda fn: os.path.getmtime(dir+'/'+fn) if not os.path.isdir(dir+'/'+fn) else 0)
+	l.reverse()
+	url = 'https://sm.ms/api/upload';
+
+	path = '/users/xuguodong/desktop/figure/';
+	url1 = []
+	for i in range(int(sys.argv[1])):
+		files = {'smfile' : open(path+l[i], 'rb')};
+
+		r = requests.post(url, files = files);
+
+		data1 = eval(r.text.encode('utf-8'));
+
+		url1.append(data1['data']['url']);
+	for i in url1:
+		print i
+
+
+
